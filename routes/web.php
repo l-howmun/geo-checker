e57d2cac5b7b4450f6901517b55df5e1b9848127
howmun <howmun@gmail.com>
2026-06-11 16:08:28 +0800

    Tighten rate limit to 3/hour

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\GeoCheckerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GeoCheckerController::class, 'index']);
-Route::post('/check', [GeoCheckerController::class, 'check'])->middleware('throttle:5,60');
+Route::post('/check', [GeoCheckerController::class, 'check'])->middleware('throttle:3,60');
